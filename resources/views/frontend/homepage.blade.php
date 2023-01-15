f787402bd4d2299533fb7ac571c23486fbe77770
Kei <budi03@gmail.com>
2023-01-16 02:30:39 +0700

    Update homepage.blade.php

--- resources/views/frontend/homepage.blade.php
+++ resources/views/frontend/homepage.blade.php
@@ -4,7 +4,19 @@
 
 @section('content')
     <div class="max-w-7xl mx-auto pt-16 sm:pt-28">
-        aaa
+        <div class="common-card-style p-4">
+            <div class="flex mb-8">
+                <div class="w-full text-right">
+                    <a href="{{ url('./') }}" target="_blank" title="{{__('New Issue')}}" class="btn">
+                        @svg('icon-add-link', '!h-[1.5em] mr-1')
+                        {{__('New Issue')}}
+                    </a>
+                </div>
+            </div>
 
+            @include('partials/messages')
+
+            @livewire('table.issue-table')
+        </div>
     </div>
 @endsection

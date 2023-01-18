e52d170f6f846c60c3bcf077bb51e98758ddc387
Kei <budi03@gmail.com>
2023-01-19 05:07:25 +0700

    U-230119050725

--- resources/views/issue/show.blade.php
+++ resources/views/issue/show.blade.php
@@ -10,30 +10,33 @@
                 {{$issue->status()}}
             </div>
             <div>
-                {{$issue->author->name}} opened this issue {{$issue->created_at->diffForHumans()}}
+                <b>{{$issue->author->name}}</b> opened this issue {{$issue->created_at->diffForHumans()}}
                 &middot; {{$issue->comments->count()}} comments
             </div>
         </div>
 
         <div class="issue_bucket flex">
             <div class="comment md:w-8/12 justify-between">
-                <div class="comment-header">
-                    <b>{{$issue->author->name}}</b> commented {{$issue->created_at->diffForHumans()}}
-                </div>
-                <div class="comment-body">
-                    {{$issue->description}}
+                <div class="issue mb-8 bg-white">
+                    <div class="comment-header">
+                        <b>{{$issue->author->name}}</b> commented {{$issue->created_at->diffForHumans()}}
+                    </div>
+                    <div class="comment-body">
+                        {{$issue->description}}
+                    </div>
                 </div>
 
                 @foreach($issue->comments->sortBy('created_at') as $comment)
-                    <div class="comment-header">
+                    <div class="comment-header mt-4">
                         <b>{{$comment->author->name}}</b> commented
-                        <a id="comment-{{$comment->id}}" href="#comment-{{$comment->id}}">{{$comment->created_at->diffForHumans()}}</a>
-
+                        <a id="comment-{{$comment->id}}" href="#comment-{{$comment->id}}" title="{{$comment->created_at->isoFormat('MMM DD, OY, HH:mm A zz')}}">
+                            {{$comment->created_at->diffForHumans()}}
+                        </a>
                         @if ($comment->isAuthor())
                             <span class="py-px px-1 border border-gray-600 rounded text-sm text-gray-600">{{$comment->userRole()}}</span>
                         @endif
                     </div>
-                    <div class="comment-body">
+                    <div class="comment-body bg-white">
                         {{$comment->text}}
                     </div>
                 @endforeach

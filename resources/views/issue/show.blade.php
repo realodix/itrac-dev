db1473c4cfa19fb30d567d8787420143d8a71396
Kei <budi03@gmail.com>
2023-01-30 02:58:20 +0700

    Update show.blade.php

--- resources/views/issue/show.blade.php
+++ resources/views/issue/show.blade.php
@@ -67,7 +67,9 @@
                     <x-easy-mde name="comment_text"/>
 
                     <x-form-button
-                        class="pointer-events-auto ml-8 rounded-md bg-indigo-600 py-2 px-3 text-[0.8125rem] font-semibold leading-5 text-white hover:bg-indigo-500"
+                        class="bg-slate-900 hover:bg-slate-700 dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400
+                            focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50
+                            text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center sm:w-auto "
                     >
                         Submit
                     </x-form-button>

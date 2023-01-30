64e7c7a3b35c6cd3e63c5cacb74416004e8275cf
Kei <budi03@gmail.com>
2023-01-31 04:13:58 +0700

    Update MarkdownService.php

--- app/Services/MarkdownService.php
+++ app/Services/MarkdownService.php
@@ -2,32 +2,20 @@
 
 namespace App\Services;
 
-use App\Models\User;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Validation\Rule;
-use Laravel\Fortify\Contracts\CreatesNewUsers;
-use Laravel\Fortify\Rules\Password;
+use League\CommonMark\CommonMarkConverter;
 
 class MarkdownService
 {
-    /**
-     * Validate and create a newly registered user.
-     *
-     * @return \App\Models\User
-     */
-    public function create(array $input)
-    {
-        Validator::make($input, [
-            'name'     => ['required', 'string', 'max:50'],
-            'email'    => ['required', 'string', 'email', 'max:255', Rule::unique(User::class)],
-            'password' => ['required', 'string', new Password, 'confirmed'],
-        ])->validate();
+    public function __construct(
+        public CommonMarkConverter $converter
+    ) {
+    }
 
-        return User::create([
-            'name'     => $input['name'],
-            'email'    => $input['email'],
-            'password' => Hash::make($input['password']),
-        ]);
+    /**
+     * @return \League\CommonMark\Output\RenderedContentInterface
+     */
+    public function handle(string $markdown)
+    {
+        return $this->converter->convert($markdown);
     }
 }

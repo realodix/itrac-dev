25596fe7dd5998ff666ccb141855eebfa5535229
Kei <budi03@gmail.com>
2023-01-31 06:51:22 +0700

    U-230131065121

--- app/Services/MarkdownService.php
+++ app/Services/MarkdownService.php
@@ -6,7 +6,6 @@ use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
-use Laravel\Fortify\Contracts\CreatesNewUsers;
 use Laravel\Fortify\Rules\Password;
 
 class MarkdownService

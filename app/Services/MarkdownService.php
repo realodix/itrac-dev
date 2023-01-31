a4615e38b29f6a6710e7fc8a79f974bd4f81cb24
Kei <budi03@gmail.com>
2023-01-31 08:19:34 +0700

    spatie/commonmark-highlighter

--- app/Services/MarkdownService.php
+++ app/Services/MarkdownService.php
@@ -2,22 +2,17 @@
 
 namespace App\Services;
 
-use League\CommonMark\Extension\GithubFlavoredMarkdownExtension;
 use League\CommonMark\Environment\Environment;
 use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
 use League\CommonMark\Extension\CommonMark\Node\Block\FencedCode;
 use League\CommonMark\Extension\CommonMark\Node\Block\IndentedCode;
+use League\CommonMark\Extension\GithubFlavoredMarkdownExtension;
 use League\CommonMark\MarkdownConverter;
 use Spatie\CommonMarkHighlighter\FencedCodeRenderer;
 use Spatie\CommonMarkHighlighter\IndentedCodeRenderer;
 
 class MarkdownService
 {
-    // public function __construct(
-    //     public GithubFlavoredMarkdownConverter $converter
-    // ) {
-    // }
-
     /**
      * Converts Markdown to HTML.
      *
@@ -27,14 +22,32 @@ class MarkdownService
      */
     public function handle(string $markdown)
     {
-        $environment = new Environment();
-        $environment->addExtension(new CommonMarkCoreExtension());
-        $environment->addRenderer(FencedCode::class, new FencedCodeRenderer());
-        $environment->addRenderer(IndentedCode::class, new IndentedCodeRenderer());
-        $environment->addExtension(new GithubFlavoredMarkdownExtension());
+        return $this->converter()->convert($markdown);
+    }
 
-        $markdownConverter = new MarkdownConverter($environment);
+    /**
+     * Returns the converter.
+     *
+     * @return \League\CommonMark\MarkdownConverter
+     */
+    private function converter()
+    {
+        return new MarkdownConverter($this->env());
+    }
 
-        return $markdownConverter->convert($markdown);
+    /**
+     * Returns the environment.
+     *
+     * @return \League\CommonMark\Environment\Environment
+     */
+    private function env()
+    {
+        $environment = new Environment;
+        $environment->addExtension(new CommonMarkCoreExtension);
+        $environment->addRenderer(FencedCode::class, new FencedCodeRenderer);
+        $environment->addRenderer(IndentedCode::class, new IndentedCodeRenderer);
+        $environment->addExtension(new GithubFlavoredMarkdownExtension);
+
+        return $environment;
     }
 }

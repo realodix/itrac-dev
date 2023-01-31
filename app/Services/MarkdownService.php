65f73831d21ad76cefa6525b3e106f4021b2b00c
Kei <budi03@gmail.com>
2023-01-31 08:01:02 +0700

    U-230131080102

--- app/Services/MarkdownService.php
+++ app/Services/MarkdownService.php
@@ -2,14 +2,21 @@
 
 namespace App\Services;
 
-use League\CommonMark\GithubFlavoredMarkdownConverter;
+use League\CommonMark\Extension\GithubFlavoredMarkdownExtension;
+use League\CommonMark\Environment\Environment;
+use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
+use League\CommonMark\Extension\CommonMark\Node\Block\FencedCode;
+use League\CommonMark\Extension\CommonMark\Node\Block\IndentedCode;
+use League\CommonMark\MarkdownConverter;
+use Spatie\CommonMarkHighlighter\FencedCodeRenderer;
+use Spatie\CommonMarkHighlighter\IndentedCodeRenderer;
 
 class MarkdownService
 {
-    public function __construct(
-        public GithubFlavoredMarkdownConverter $converter
-    ) {
-    }
+    // public function __construct(
+    //     public GithubFlavoredMarkdownConverter $converter
+    // ) {
+    // }
 
     /**
      * Converts Markdown to HTML.
@@ -20,6 +27,14 @@ class MarkdownService
      */
     public function handle(string $markdown)
     {
-        return $this->converter->convert($markdown);
+        $environment = new Environment();
+        $environment->addExtension(new CommonMarkCoreExtension());
+        $environment->addRenderer(FencedCode::class, new FencedCodeRenderer());
+        $environment->addRenderer(IndentedCode::class, new IndentedCodeRenderer());
+        $environment->addExtension(new GithubFlavoredMarkdownExtension());
+
+        $markdownConverter = new MarkdownConverter($environment);
+
+        return $markdownConverter->convert($markdown);
     }
 }

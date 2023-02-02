c4777726dabcd37fc5b6131e64e02493567740a9
Kei <budi03@gmail.com>
2023-02-02 14:10:47 +0700

    Update Comment.php

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -59,7 +59,7 @@ class Comment extends Model
      */
     public function isAuthor(): bool
     {
-        return auth()->id() === $this->author_id;
+        return $this->author->id === $this->author_id;
     }
 
     /**

749f4c3a7296d53bb093e1d0c4c222f917e02dc3
Kei <budi03@gmail.com>
2023-02-02 15:22:44 +0700

    Update show.blade.php

--- resources/views/issue/show.blade.php
+++ resources/views/issue/show.blade.php
@@ -19,16 +19,26 @@
         <div class="issue_bucket flex">
             <div class="md:w-8/12 justify-between">
                 <div class="comment mb-8">
-                    <div class="comment-header">
-                        <img src="{{ Avatar::create($issue->author->name)->toBase64() }}" class="h-8 inline-block" />
-                        <b>{{$issue->author->name}}</b> commented
-                        <a id="issue-{{$issue->id}}" href="#issue-{{$issue->id}}" title="{{$issue->created_at->isoFormat('MMM DD, OY, HH:mm A zz')}}">
-                            {{$issue->created_at->diffForHumans()}}
-                        </a>
+                    <div class="comment-header grid grid-cols-2 content-center">
+                        <div class="flex items-center space-x-4">
+                            <img class="w-10 h-10 rounded-full" src="{{ Avatar::create($issue->author->name)->toBase64() }}"/>
+                            <div>
+                                <b>{{$issue->author->name}}</b>
+                                <div class="text-sm text-gray-500">
+                                    commented
+                                    <a id="issue-{{$issue->id}}" href="#issue-{{$issue->id}}" title="{{$issue->created_at->isoFormat('MMM DD, OY, HH:mm A zz')}}">
+                                        {{$issue->created_at->diffForHumans()}}
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+
                         @auth
-                            @if ($issue->isAuthor() || auth()->user()->hasRole('admin'))
-                                <a href="{{route('issue.edit', $issue)}}" class="font-semibold">Edit</a>
-                            @endif
+                        @if ($issue->isAuthor() || auth()->user()->hasRole('admin'))
+                            <div class="flex justify-end flex-wrap content-center">
+                                <a href="{{route('issue.edit', $issue)}}" class="px-4 py-2 mr-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200">Edit</a>
+                            </div>
+                        @endif
                         @endauth
                     </div>
                     <x-markdown class="comment-body markdown">{!! $issue->description !!}</x-markdown>
@@ -39,13 +49,11 @@
                         <div class="comment-header grid grid-cols-2 content-center">
                             <div class="flex items-center space-x-4">
                                 <img class="w-10 h-10 rounded-full" src="{{ Avatar::create($comment->author->name)->toBase64() }}" alt="">
-                                <div class="font-medium dark:text-white">
-                                    <div>
-                                        {{$comment->author->name}}
-                                        @if ($comment->isAuthor())
-                                            <span class="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded border border-green-400">{{$comment->userRole()}}</span>
-                                        @endif
-                                    </div>
+                                <div>
+                                    <b>{{$comment->author->name}}</b>
+                                    @if ($comment->isAuthor())
+                                        <span class="bg-green-100 text-green-800 text-xs mr-2 px-2.5 py-0.5 rounded border border-green-400">{{$comment->userRole()}}</span>
+                                    @endif
                                     <div class="text-sm text-gray-500">
                                         commented
                                         <a id="comment-{{$comment->id}}"
@@ -59,8 +67,8 @@
                             @auth
                             @if ($comment->isAuthor() || auth()->user()->hasRole('admin'))
                             <div class="flex justify-end flex-wrap content-center">
-                                <a href="{{route('comment.edit', $comment)}}" class="px-4 py-2 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200">Edit</a>
-                                <a href="{{route('comment.delete', $comment)}}" class="px-4 py-2 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200">Delete</a>
+                                <a href="{{route('comment.edit', $comment)}}" class="px-4 py-2 mr-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200">Edit</a>
+                                <a href="{{route('comment.delete', $comment)}}" class="px-4 py-2 mr-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200">Delete</a>
                             </div>
                             @endif
                             @endauth

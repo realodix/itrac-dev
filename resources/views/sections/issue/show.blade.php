7b976f038574a383cd4cfa0292b9531b52960d3c
Kei <budi03@gmail.com>
2023-02-03 04:04:54 +0700

    Tombol cooment action sebagai blade component

--- resources/views/sections/issue/show.blade.php
+++ resources/views/sections/issue/show.blade.php
@@ -45,7 +45,11 @@
                         @auth
                         @if ($issue->isAuthor() || auth()->user()->hasRole('admin'))
                             <div class="flex justify-end flex-wrap content-center">
-                                <a href="{{route('issue.edit', $issue)}}" class="comment-header-btn">Edit</a>
+                                <x-comment-action>
+                                    <a href="{{route('issue.edit', $issue)}}"
+                                        class="flex items-center gap-2 w-full first-of-type:rounded-t-md last-of-type:rounded-b-md px-4 py-2.5 text-left text-sm hover:bg-gray-50 disabled:text-gray-500"
+                                        >Edit</a>
+                                </x-comment-action>
                             </div>
                         @endif
                         @endauth
@@ -77,61 +81,14 @@
                                         <span class="bg-green-100 text-green-800 text-xs mr-2 px-2.5 py-0.5 rounded border border-green-400">
                                             {{$comment->userRole()}}</span>
                                     @endif
-                                    <div
-                                        x-data="{
-                                            open: false,
-                                            toggle() {
-                                                if (this.open) {
-                                                    return this.close()
-                                                }
-
-                                                this.$refs.button.focus()
-
-                                                this.open = true
-                                            },
-                                            close(focusAfter) {
-                                                if (! this.open) return
-
-                                                this.open = false
-
-                                                focusAfter && focusAfter.focus()
-                                            }
-                                        }"
-                                        x-on:keydown.escape.prevent.stop="close($refs.button)"
-                                        x-on:focusin.window="! $refs.panel.contains($event.target) && close()"
-                                        x-id="['dropdown-button']"
-                                        class="relative"
-                                    >
-                                        <!-- Button -->
-                                        <button
-                                            x-ref="button"
-                                            x-on:click="toggle()"
-                                            :aria-expanded="open"
-                                            :aria-controls="$id('dropdown-button')"
-                                            type="button"
-                                            {{-- class="flex items-center gap-2 bg-white px-5 py-2.5 rounded-md shadow" --}}
-                                        >
-                                            <svg class="blade-icon" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16"><path fill-rule="evenodd" clip-rule="evenodd" d="M7.444 13.832a1 1 0 1 0 1.111-1.663 1 1 0 0 0-1.11 1.662zM8 9a1 1 0 1 1 0-2 1 1 0 0 1 0 2zm0-5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"></path></svg>
-                                        </button>
-
-                                        <!-- Panel -->
-                                        <div
-                                            x-ref="panel"
-                                            x-show="open"
-                                            x-transition.origin.top.left
-                                            x-on:click.outside="close($refs.button)"
-                                            :id="$id('dropdown-button')"
-                                            style="display: none;"
-                                            class="absolute left-0 mt-2 w-40 rounded-md bg-white shadow-md"
-                                        >
-                                            <a href="{{route('comment.edit', $comment)}}"
-                                                class="flex items-center gap-2 w-full first-of-type:rounded-t-md last-of-type:rounded-b-md px-4 py-2.5 text-left text-sm hover:bg-gray-50 disabled:text-gray-500"
-                                                >Edit</a>
-                                            <a href="{{route('comment.delete', $comment)}}"
-                                                class="flex items-center gap-2 w-full first-of-type:rounded-t-md last-of-type:rounded-b-md px-4 py-2.5 text-left text-sm hover:bg-gray-50 disabled:text-gray-500"
-                                                ><span class="text-red-600">Delete</span></a>
-                                        </div>
-                                    </div>
+                                    <x-comment-action>
+                                        <a href="{{route('comment.edit', $comment)}}"
+                                            class="flex items-center gap-2 w-full first-of-type:rounded-t-md last-of-type:rounded-b-md px-4 py-2.5 text-left text-sm hover:bg-gray-50 disabled:text-gray-500"
+                                            >Edit</a>
+                                        <a href="{{route('comment.delete', $comment)}}"
+                                            class="flex items-center gap-2 w-full first-of-type:rounded-t-md last-of-type:rounded-b-md px-4 py-2.5 text-left text-sm hover:bg-gray-50 disabled:text-gray-500"
+                                            ><span class="text-red-600">Delete</span></a>
+                                    </x-comment-action>
                                 </div>
                             @endif
                             @endauth

8c1d6392bed61362f4a094e613253c399642cf4d
Kei <budi03@gmail.com>
2023-02-02 23:39:09 +0700

    Update ChangePasswordTest.php

--- tests/Feature/AuthPage/User/ChangePasswordTest.php
+++ tests/Feature/AuthPage/User/ChangePasswordTest.php
@@ -20,6 +20,22 @@ class ChangePasswordTest extends TestCase
         return route('user.change-password.post', $hashids->encode($value));
     }
 
+
+    /**
+     * User can access the change password page.
+     * @test
+     * @group f-user
+     */
+    public function userCanAccessTheChangePasswordPage()
+    {
+        $user = $this->normalUser();
+
+        $response = $this->actingAs($user)
+            ->get($this->getRoute($user->name));
+
+        $response->assertOk();
+    }
+
     /**
      * @test
      * @group f-user

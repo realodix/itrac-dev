54cc5bc1263b313c3df96d803d8308a4a2cfd69c
Kei <budi03@gmail.com>
2023-02-08 14:18:08 +0700

    Update AuthServiceProvider.php

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -14,6 +14,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         \App\Models\User::class => \App\Policies\UserPolicy::class,
+        \App\Models\Issue::class => \App\Policies\IssuePolicy::class,
         \App\Models\Comment::class => \App\Policies\CommentPolicy::class,
     ];
 

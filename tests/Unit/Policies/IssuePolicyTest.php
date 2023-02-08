879ebe5f2d65ff5a74adbdbd021b08ba4402666e
Kei <budi03@gmail.com>
2023-02-08 10:17:56 +0700

    Update IssuePolicyTest.php

--- tests/Unit/Policies/IssuePolicyTest.php
+++ tests/Unit/Policies/IssuePolicyTest.php
@@ -289,4 +289,146 @@ class IssuePolicyTest extends TestCase
         $response = $this->get(route('issue.reopen', $issue));
         $response->assertForbidden();
     }
+
+    /**
+     * Issue author can lock the issue.
+     * Author is the user who created the issue.
+     *
+     * @test
+     * @group u-policy
+     */
+    public function authorCanLockIssue()
+    {
+        $issue = Issue::factory()->create();
+
+        $this->assertTrue($issue->author->can('lock', $issue));
+
+        $response = $this->actingAs($issue->author)
+            ->get(route('issue.lock', $issue));
+        $response->assertStatus(302);
+    }
+
+    /**
+     * Admin can lock the issue.
+     * Admin is the user who has the admin role.
+     *
+     * @test
+     * @group u-policy
+     */
+    public function adminCanLockIssue()
+    {
+        $issue = Issue::factory()->create();
+        $adminUser = $this->adminUser();
+
+        $this->assertTrue($adminUser->can('lock', $issue));
+
+        $response = $this->actingAs($adminUser)
+            ->get(route('issue.lock', $issue));
+        $response->assertStatus(302);
+    }
+
+    /**
+     * Non-author and non-admin cannot lock the issue.
+     * Non-author is the user who did not create the issue.
+     *
+     * @test
+     * @group u-policy
+     */
+    public function nonAuthorCannotLockIssue()
+    {
+        $user = User::factory()->create();
+        $issue = Issue::factory()->create();
+
+        $this->assertFalse($user->can('lock', $issue));
+
+        $response = $this->actingAs($user)
+            ->get(route('issue.lock', $issue));
+        $response->assertForbidden();
+    }
+
+    /**
+     * Guest cannot lock the issue.
+     * Guest is the user who is not logged in.
+     *
+     * @test
+     * @group u-policy
+     */
+    public function guestCannotLockIssue()
+    {
+        $issue = Issue::factory()->create();
+
+        $response = $this->get(route('issue.lock', $issue));
+        $response->assertForbidden();
+    }
+
+    /**
+     * Issue author can unlock the issue.
+     * Author is the user who created the issue.
+     *
+     * @test
+     * @group u-policy
+     */
+    public function authorCanUnlockIssue()
+    {
+        $issue = Issue::factory()->create();
+
+        $this->assertTrue($issue->author->can('unlock', $issue));
+
+        $response = $this->actingAs($issue->author)
+            ->get(route('issue.unlock', $issue));
+        $response->assertStatus(302);
+    }
+
+    /**
+     * Admin can unlock the issue.
+     * Admin is the user who has the admin role.
+     *
+     * @test
+     * @group u-policy
+     */
+    public function adminCanUnlockIssue()
+    {
+        $issue = Issue::factory()->create();
+        $adminUser = $this->adminUser();
+
+        $this->assertTrue($adminUser->can('unlock', $issue));
+
+        $response = $this->actingAs($adminUser)
+            ->get(route('issue.unlock', $issue));
+        $response->assertStatus(302);
+    }
+
+    /**
+     * Non-author and non-admin cannot unlock the issue.
+     * Non-author is the user who did not create the issue.
+     *
+     * @test
+     * @group u-policy
+     */
+    public function nonAuthorCannotUnlockIssue()
+    {
+        $user = User::factory()->create();
+        $issue = Issue::factory()->create();
+
+        $this->assertFalse($user->can('unlock', $issue));
+
+        $response = $this->actingAs($user)
+            ->get(route('issue.unlock', $issue));
+        $response->assertForbidden();
+    }
+
+    /**
+     * Guest cannot unlock the issue.
+     * Guest is the user who is not logged in.
+     *
+     * @test
+     * @group u-policy
+     */
+    public function guestCannotUnlockIssue()
+    {
+        $issue = Issue::factory()->create();
+
+        $response = $this->get(route('issue.unlock', $issue));
+        $response->assertForbidden();
+    }
 }

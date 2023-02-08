a43cfc9f4ec2cee4dd74d6cbf0e735f4a0001c78
Kei <budi03@gmail.com>
2023-02-08 08:04:55 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -27,7 +27,7 @@ class DatabaseSeeder extends Seeder
         // Multiple with factory
         Issue::factory(3)
             ->for(User::factory()->create(), 'author')
-            ->has(Comment::factory(25))
+            ->has(Comment::factory(5))
             ->create();
     }
 }

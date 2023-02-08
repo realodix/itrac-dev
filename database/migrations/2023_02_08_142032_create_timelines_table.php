7aa74bb645431cef5462493cd69f4dd246689bec
Kei <budi03@gmail.com>
2023-02-08 21:35:54 +0700

    Timeline

---
+++ database/migrations/2023_02_08_142032_create_timelines_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('timelines', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('issue_id')
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->foreignId('author_id')
+                ->constrained('users')
+                ->cascadeOnDelete();
+            $table->string('type');
+            $table->longText('summary');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('timelines');
+    }
+};

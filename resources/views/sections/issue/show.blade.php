ae916d615ac79b4d82b07f489b92cdbdb0a5bdd5
Kei <budi03@gmail.com>
2023-02-08 20:13:12 +0700

    Update show.blade.php

--- resources/views/sections/issue/show.blade.php
+++ resources/views/sections/issue/show.blade.php
@@ -115,9 +115,52 @@
                         <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
                             <svg aria-hidden="true" class="w-3 h-3 text-blue-800 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
                         </span>
-                        <h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">Flowbite Figma v1.3.0</h3>
-                        <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Released on December 7th, 2021</time>
                         <p class="text-base font-normal text-gray-500 dark:text-gray-400">All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.</p>
+                    </li>
+                    <li class="mb-10 ml-6">
+                        <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+                            <svg aria-hidden="true" class="w-3 h-3 text-blue-800 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                        </span>
+                        <div class="comment">
+                            <div class="comment-header grid grid-cols-2 content-center">
+                                <div class="flex items-center space-x-4">
+                                    <img src="{{ Avatar::create($comment->author->name)->toBase64() }}" class="comment-header-avatar"/>
+                                    <div>
+                                        <b>{{$comment->author->name}}</b>
+                                        <div class="text-sm text-gray-500">
+                                            commented
+                                            <a href="#comment-{{$comment->id}}"
+                                                id="comment-{{$comment->id}}"
+                                                title="{{$comment->created_at->isoFormat('MMM DD, OY, hh:mm A zz')}}"
+                                                >{{$comment->created_at->diffForHumans()}}</a>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                @auth
+                                    <div class="flex justify-end flex-wrap content-center">
+                                        @if ($comment->isIssueAuthor())
+                                            <span class="bg-green-100 text-green-800 text-xs mr-2 px-2.5 py-0.5 rounded border border-green-400">
+                                                {{$comment->userRole()}}</span>
+                                        @endif
+
+                                        @if ($comment->isAuthor() || auth()->user()->hasRole('admin'))
+                                            <x-comment-action>
+                                                <x-comment-action-item>
+                                                    <a href="{{route('comment.edit', $comment)}}">Edit</a>
+                                                </x-comment-action-item>
+                                                <x-comment-action-item>
+                                                    <a href="{{route('comment.delete', $comment)}}">
+                                                        <span class="text-red-600">Delete</span></a>
+                                                </x-comment-action-item>
+                                            </x-comment-action>
+                                        @endif
+
+                                    </div>
+                                @endauth
+                            </div>
+                            <x-markdown class="comment-body markdown">{!! $comment->description !!}</x-markdown>
+                        </div>
                     </li>
                 </ol>
 

18f067eb70a1bfbcf562af74f8e87c06556771c7
Kei <budi03@gmail.com>
2023-02-08 16:21:08 +0700

    Update show.blade.php

--- resources/views/sections/issue/show.blade.php
+++ resources/views/sections/issue/show.blade.php
@@ -143,34 +143,34 @@
                     </div>
                 </div>
                 @auth
-                    @if ($issue->isAuthor() || auth()->user()->hasRole('admin'))
-                        <div class="participation discussion-sidebar-item">
-                            <div class="flex flex-col">
-                                <div>
-                                    @if ($issue->isLocked())
-                                        <x-go-unlock-16 />
-                                        <a href="{{route('issue.unlock', $issue)}}" class="font-semibold">Unlock conversation</a>
-                                    @else
-                                        <x-go-lock-16 />
-                                        <a href="{{route('issue.lock', $issue)}}" class="font-semibold">Lock conversation</a>
-                                    @endif
-                                </div>
-                                <div>
-                                    @if ($issue->isClosed())
-                                        <x-go-issue-reopened-16 class="text-green-600" />
-                                        <a href="{{route('issue.reopen', $issue)}}" class="font-semibold">Reopen</a>
-                                    @else
-                                        <x-go-issue-closed-16 class="text-violet-700" />
-                                        <a href="{{route('issue.close', $issue)}}" class="font-semibold">Close</a>
-                                    @endif
-                                </div>
-                                <div class="mt-4 text-red-600">
-                                    <x-go-trash-16 />
-                                    <a href="{{route('issue.delete', $issue)}}" class="font-semibold">Delete issue</a>
-                                </div>
+                @if ($issue->isAuthor() || auth()->user()->hasRole('admin'))
+                    <div class="participation discussion-sidebar-item text-sm">
+                        <div class="flex flex-col">
+                            <div>
+                                @if ($issue->isLocked())
+                                    <x-go-unlock-16 />
+                                    <a href="{{route('issue.unlock', $issue)}}" class="font-semibold">Unlock conversation</a>
+                                @else
+                                    <x-go-lock-16 />
+                                    <a href="{{route('issue.lock', $issue)}}" class="font-semibold">Lock conversation</a>
+                                @endif
+                            </div>
+                            <div class="mt-4">
+                                @if ($issue->isClosed())
+                                    <x-go-issue-reopened-16 class="text-green-600" />
+                                    <a href="{{route('issue.reopen', $issue)}}" class="font-semibold">Reopen</a>
+                                @else
+                                    <x-go-issue-closed-16 class="text-violet-700" />
+                                    <a href="{{route('issue.close', $issue)}}" class="font-semibold">Close</a>
+                                @endif
+                            </div>
+                            <div class="mt-4 text-red-600">
+                                <x-go-trash-16 />
+                                <a href="{{route('issue.delete', $issue)}}" class="font-semibold">Delete issue</a>
                             </div>
                         </div>
-                    @endif
+                    </div>
+                @endif
                 @endauth
             </div>
         </div>

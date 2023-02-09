e4a56807c71d617c1c3e369926e323e9100251a3
Kei <budi03@gmail.com>
2023-02-09 19:22:05 +0700

    U-230209192717

--- resources/views/sections/issue/show.blade.php
+++ resources/views/sections/issue/show.blade.php
@@ -3,40 +3,40 @@
 @section('css_class', 'frontend home')
 
 @section('content')
-    <div class="issue">
-        <div class="header-meta">
-            <div class="text-3xl mb-2">#{{$issue->id}} - {{$issue->title}}</div>
-            <div class="mb-2">
-                <span @class([
-                        'mr-4 py-1 px-2 border rounded-md text-md text-white',
-                        'bg-green-600 border-green-600' => ! $issue->isClosed(),
-                        'bg-violet-700 border-violet-700' => $issue->isClosed(),
-                    ])>
-                    @if ($issue->isClosed())
-                       <x-go-issue-closed-16 />
-                    @else
-                        <x-go-issue-opened-16 />
-                    @endif
-                    {{$issue->status()}}
-                </span>
-                <b>{{$issue->author->name}}</b> opened this issue <span title="{{$issue->created_at->isoFormat('MMM DD, OY, hh:mm A zz')}}">{{$issue->created_at->diffForHumans()}}</span>
-                &middot; {{ $issue->commentCount(); }} comments
-            </div>
-        </div>
-
-        <div class="issue_bucket flex">
-            <div class="md:w-8/12 justify-between">
-                @include('sections.issue.subviews.summary')
-
-                @include('sections.issue.subviews.timeline')
-
-                <br>
-
-                @include('sections.issue.subviews.comment-form')
-            </div>
-            <div class="md:w-4/12 px-4 py-2">
-                @include('sections.issue.subviews.sidebar')
-            </div>
+<div class="issue">
+    <div class="header">
+        <div class="text-3xl mb-2">#{{$issue->id}} - {{$issue->title}}</div>
+        <div class="mb-2">
+            <span @class([
+                    'mr-4 py-1 px-2 border rounded-md text-md text-white',
+                    'bg-green-600 border-green-600' => ! $issue->isClosed(),
+                    'bg-violet-700 border-violet-700' => $issue->isClosed(),
+                ])>
+                @if ($issue->isClosed())
+                    <x-go-issue-closed-16 />
+                @else
+                    <x-go-issue-opened-16 />
+                @endif
+                {{$issue->status()}}
+            </span>
+            <b>{{$issue->author->name}}</b> opened this issue <span title="{{$issue->created_at->isoFormat('MMM DD, OY, hh:mm A zz')}}">{{$issue->created_at->diffForHumans()}}</span>
+            &middot; {{ $issue->commentCount(); }} comments
         </div>
     </div>
+
+    <div class="bucket">
+        <div class="layout-main">
+            @include('sections.issue.subviews.summary')
+
+            @include('sections.issue.subviews.timeline')
+
+            <br>
+
+            @include('sections.issue.subviews.comment-form')
+        </div>
+        <div class="layout-sidebar">
+            @include('sections.issue.subviews.sidebar')
+        </div>
+    </div>
+</div>
 @endsection
--- resources/views/sections/issue/subviews/timeline.blade.php
+++ resources/views/sections/issue/subviews/timeline.blade.php
@@ -2,58 +2,58 @@
 {{-- https://flowbite.com/docs/components/timeline/#vertical-timeline --}}
 <ol class="timeline">
 @foreach($issue->comments->sortBy('created_at') as $comment)
-    @if ($comment->isComment())
-        <li class="mb-10 -ml-6">
-            <div class="comment shadow">
-                <div class="comment-header grid grid-cols-2 content-center">
-                    <div class="flex items-center space-x-4">
-                        <img src="{{ Avatar::create($comment->author->name)->toBase64() }}" class="comment-header-avatar"/>
-                        <div>
-                            <b>{{$comment->author->name}}</b>
-                            <div class="text-sm text-gray-500">
-                                commented
-                                <a href="#comment-{{$comment->id}}"
-                                    id="comment-{{$comment->id}}"
-                                    title="{{$comment->created_at->isoFormat('MMM DD, OY, hh:mm A zz')}}"
-                                    >{{$comment->created_at->diffForHumans()}}</a>
-                            </div>
+@if ($comment->isComment())
+    <li class="mb-10 -ml-6">
+        <div class="comment shadow">
+            <div class="comment-header grid grid-cols-2 content-center">
+                <div class="flex items-center space-x-4">
+                    <img src="{{ Avatar::create($comment->author->name)->toBase64() }}" class="comment-header-avatar"/>
+                    <div>
+                        <b>{{$comment->author->name}}</b>
+                        <div class="text-sm text-gray-500">
+                            commented
+                            <a href="#comment-{{$comment->id}}"
+                                id="comment-{{$comment->id}}"
+                                title="{{$comment->created_at->isoFormat('MMM DD, OY, hh:mm A zz')}}"
+                                >{{$comment->created_at->diffForHumans()}}</a>
                         </div>
                     </div>
-
-                    @auth
-                    <div class="flex justify-end flex-wrap content-center">
-                        @if ($comment->isIssueAuthor())
-                        <span class="bg-green-100 text-green-800 text-xs mr-2 px-2.5 py-0.5 rounded border border-green-400">
-                            {{$comment->userRole()}}</span>
-                        @endif
-
-                        @if ($comment->isAuthor() || auth()->user()->hasRole('admin'))
-                        <x-comment-action>
-                            <x-comment-action-item>
-                                <a href="{{route('comment.edit', $comment)}}">Edit</a>
-                            </x-comment-action-item>
-                            <x-comment-action-item>
-                                <a href="{{route('comment.delete', $comment)}}">
-                                    <span class="text-red-600">Delete</span></a>
-                            </x-comment-action-item>
-                        </x-comment-action>
-                        @endif
-
-                    </div>
-                    @endauth
                 </div>
-                <x-markdown class="comment-body markdown">{!! $comment->description !!}</x-markdown>
+
+                @auth
+                <div class="flex justify-end flex-wrap content-center">
+                    @if ($comment->isIssueAuthor())
+                    <span class="bg-green-100 text-green-800 text-xs mr-2 px-2.5 py-0.5 rounded border border-green-400">
+                        {{$comment->userRole()}}</span>
+                    @endif
+
+                    @if ($comment->isAuthor() || auth()->user()->hasRole('admin'))
+                    <x-comment-action>
+                        <x-comment-action-item>
+                            <a href="{{route('comment.edit', $comment)}}">Edit</a>
+                        </x-comment-action-item>
+                        <x-comment-action-item>
+                            <a href="{{route('comment.delete', $comment)}}">
+                                <span class="text-red-600">Delete</span></a>
+                        </x-comment-action-item>
+                    </x-comment-action>
+                    @endif
+
+                </div>
+                @endauth
             </div>
-        </li>
-    @else
-        <li class="mb-6 ml-6">
-            <span class="absolute flex items-center justify-center w-7 h-7 bg-teal-600 rounded-full -left-4">
-                <x-go-info-16 class="w-6 h-6 text-white" />
-            </span>
-            <p class="text-sm font-normal text-gray-500">
-                <b class="text-[#1F2937]">{!! $comment->author->name !!}</b> {!! $comment->description !!} {!! $comment->created_at->diffForHumans() !!}
-            </p>
-        </li>
-    @endif
+            <x-markdown class="comment-body markdown">{!! $comment->description !!}</x-markdown>
+        </div>
+    </li>
+@else
+    <li class="mb-6 ml-6">
+        <span class="absolute flex items-center justify-center w-7 h-7 bg-teal-600 rounded-full -left-4">
+            <x-go-info-16 class="w-6 h-6 text-white" />
+        </span>
+        <p class="text-sm font-normal text-gray-500">
+            <b class="text-[#1F2937]">{!! $comment->author->name !!}</b> {!! $comment->description !!} {!! $comment->created_at->diffForHumans() !!}
+        </p>
+    </li>
+@endif
 @endforeach
 </ol>

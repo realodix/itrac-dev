e151b15d3282cc860ed196d66ddaaf7442295dfe
Kei <budi03@gmail.com>
2023-02-15 14:23:30 +0700

    Update HashidsService.php

--- app/Services/HashidsService.php
+++ app/Services/HashidsService.php
@@ -10,7 +10,7 @@ class HashidsService
 
     public function __construct()
     {
-        $this->hashids = new Hashids(minHashLength: 6);
+        $this->hashids = new Hashids(minHashLength: 16);
     }
 
     /**

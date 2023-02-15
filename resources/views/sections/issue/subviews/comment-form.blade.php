5b45325ac0c8feaf135cef1e3e59601cd1ebc606
Kei <budi03@gmail.com>
2023-02-15 23:36:08 +0700

    Blade Icons (local) (#12)

--- resources/views/sections/issue/subviews/comment-form.blade.php
+++ resources/views/sections/issue/subviews/comment-form.blade.php
@@ -1,7 +1,7 @@
 @if (auth()->check())
     @if ($issue->isLocked() && ! $issue->isAuthor() && ! $issue->isParticipant() && ! auth()->user()->hasRole('admin'))
         <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-            <x-go-lock-16 />
+            <x-icon-lock-16 />
 
             <b>{{$issue->lockedBy->name}}</b> locked this issue and limited conversations to collaborators {{$issue->locked_at->diffForHumans()}}.
         </div>
--- resources/views/sections/issue/subviews/sidebar.blade.php
+++ resources/views/sections/issue/subviews/sidebar.blade.php
@@ -19,24 +19,24 @@
     <div class="flex flex-col">
         <div>
             @if ($issue->isLocked())
-                <x-go-unlock-16 />
+                <x-icon-unlock-16 />
                 <a href="{{route('issue.unlock', $issue)}}" class="font-semibold">Unlock conversation</a>
             @else
-                <x-go-lock-16 />
+                <x-icon-lock-16 />
                 <a href="{{route('issue.lock', $issue)}}" class="font-semibold">Lock conversation</a>
             @endif
         </div>
         <div class="mt-4">
             @if ($issue->isClosed())
-                <x-go-issue-reopened-16 />
+                <x-icon-issue-reopened-16 />
                 <a href="{{route('issue.reopen', $issue)}}" class="font-semibold">Reopen</a>
             @else
-                <x-go-issue-closed-16 />
+                <x-icon-issue-closed-16 />
                 <a href="{{route('issue.close', $issue)}}" class="font-semibold">Close</a>
             @endif
         </div>
         <div class="mt-4 text-red-600">
-            <x-go-trash-16 />
+            <x-icon-trash-16 />
             <a href="{{route('issue.delete', $issue)}}" class="font-semibold">Delete issue</a>
         </div>
     </div>

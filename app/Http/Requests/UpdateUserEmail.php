8d312ca9e2abb13583cbf801f4c0b1957046b2cb
Kei <budi03@gmail.com>
2023-02-18 00:13:48 +0700

    app\Http\Requests

--- app/Http/Requests/UpdateUserEmail.php
+++ app/Http/Requests/UpdateUserEmail.php
@@ -8,10 +8,8 @@ class UpdateUserEmail extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
-     *
-     * @return bool
      */
-    public function authorize()
+    public function authorize(): bool
     {
         return true;
     }
@@ -19,9 +17,9 @@ class UpdateUserEmail extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, mixed>
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
      */
-    public function rules()
+    public function rules(): array
     {
         return [
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],

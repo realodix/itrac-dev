e535ce186a4bc1655deb8a53678854905517b41a
Kei <budi03@gmail.com>
2023-02-20 10:35:43 +0700

    Update sidebar.blade.php

--- resources/views/sections/issue/subviews/sidebar.blade.php
+++ resources/views/sections/issue/subviews/sidebar.blade.php
@@ -13,43 +13,45 @@
 </div>
 
 @auth
-<div class="participation sidebar-item">
-    <div>
-        @svg('icon-edit')
-        <a href="{{route('issue.edit', $issue)}}">Edit title</a>
-    </div>
-    <div class="mb-2 mt-2">
-        @svg('icon-edit')
-        <a href="{{route('issue.edit', $issue)}}">Edit description</a>
-    </div>
-</div>
+    @if ($issue->isAuthor() || auth()->user()->hasRole('admin'))
+        <div class="participation sidebar-item">
+            <div>
+                @svg('icon-edit')
+                <a href="{{route('issue.edit', $issue)}}">Edit title</a>
+            </div>
+            <div class="mb-2 mt-2">
+                @svg('icon-edit')
+                <a href="{{route('issue.edit', $issue)}}">Edit description</a>
+            </div>
+        </div>
+    @endif
 
-@if ($issue->isAuthor() || auth()->user()->hasRole('admin'))
-<div class="participation sidebar-item text-sm">
-    <div class="flex flex-col">
-        <div>
-            @if ($issue->isLocked())
-                <x-icon-unlock-16 />
-                <a href="{{route('issue.unlock', $issue)}}" class="font-semibold">Unlock conversation</a>
-            @else
-                <x-icon-lock-16 />
-                <a href="{{route('issue.lock', $issue)}}" class="font-semibold">Lock conversation</a>
-            @endif
+    @if ($issue->isAuthor() || auth()->user()->hasRole('admin'))
+        <div class="participation sidebar-item text-sm">
+            <div class="flex flex-col">
+                <div>
+                    @if ($issue->isLocked())
+                        <x-icon-unlock-16 />
+                        <a href="{{route('issue.unlock', $issue)}}" class="font-semibold">Unlock conversation</a>
+                    @else
+                        <x-icon-lock-16 />
+                        <a href="{{route('issue.lock', $issue)}}" class="font-semibold">Lock conversation</a>
+                    @endif
+                </div>
+                <div class="mt-2">
+                    @if ($issue->isClosed())
+                        <x-icon-issue-reopened-16 />
+                        <a href="{{route('issue.reopen', $issue)}}" class="font-semibold">Reopen</a>
+                    @else
+                        <x-icon-issue-closed-16 />
+                        <a href="{{route('issue.close', $issue)}}" class="font-semibold">Close</a>
+                    @endif
+                </div>
+                <div class="mt-2 text-red-600">
+                    <x-icon-trash-16 />
+                    <a href="{{route('issue.delete', $issue)}}" class="font-semibold">Delete issue</a>
+                </div>
+            </div>
         </div>
-        <div class="mt-2">
-            @if ($issue->isClosed())
-                <x-icon-issue-reopened-16 />
-                <a href="{{route('issue.reopen', $issue)}}" class="font-semibold">Reopen</a>
-            @else
-                <x-icon-issue-closed-16 />
-                <a href="{{route('issue.close', $issue)}}" class="font-semibold">Close</a>
-            @endif
-        </div>
-        <div class="mt-2 text-red-600">
-            <x-icon-trash-16 />
-            <a href="{{route('issue.delete', $issue)}}" class="font-semibold">Delete issue</a>
-        </div>
-    </div>
-</div>
-@endif
+    @endif
 @endauth

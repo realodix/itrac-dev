69248277d090b8cb636e72bcd5bfe0a12c035a71
Kei <budi03@gmail.com>
2023-02-21 00:44:38 +0700

    Create 2023_02_20_174313_create_history_table.php

---
+++ database/migrations/2023_02_20_174313_create_history_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('history', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('author_id')
+                ->constrained('users')
+                ->cascadeOnDelete();
+            $table->string('event');
+            $table->morphs('auditable');
+            $table->text('old_values')->nullable();
+            $table->text('new_values')->nullable();
+            $table->string('tags')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('history');
+    }
+};

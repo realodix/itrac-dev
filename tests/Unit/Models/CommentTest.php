9b4086b692793dba04295a2790c1626df2c9a9cf
Kei <budi03@gmail.com>
2023-02-22 23:44:16 +0700

    Update CommentTest.php

--- tests/Unit/Models/CommentTest.php
+++ tests/Unit/Models/CommentTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit\Models;
 
+use App\Enums\CommentType;
 use App\Models\Comment;
 use App\Models\User;
 use Tests\TestCase;
@@ -37,7 +38,38 @@ class CommentTest extends TestCase
     }
 
     /**
-     * Is the comment a comment?
+     * Determine if the comment was written by the current authenticated user.
+     *
+     * @test
+     * @group u-model
+     */
+    public function isAuthor()
+    {
+        $c = Comment::factory()->create();
+        $this->assertFalse($c->isAuthor());
+
+        $this->actingAs($c->author);
+        $this->assertTrue($c->isAuthor());
+    }
+
+    /**
+     * Determine if the comment was written by the issue author.
+     *
+     * @test
+     * @group u-model
+     */
+    public function isIssueAuthor()
+    {
+        $c = Comment::factory()->create();
+        $this->assertFalse($c->isIssueAuthor());
+
+        $c->issue->author()->associate($c->author);
+        $c->issue->save();
+        $this->assertTrue($c->isIssueAuthor());
+    }
+
+    /**
+     * Determine if the type of the comment is a comment.
      *
      * @test
      * @group u-model
@@ -48,7 +80,7 @@ class CommentTest extends TestCase
         $this->assertTrue($c->isComment());
 
         $c = Comment::factory()->create([
-            'type' => false,
+            'type' => CommentType::Revision->value,
         ]);
         $this->assertFalse($c->isComment());
     }

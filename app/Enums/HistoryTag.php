57c3040374b1fbc92a4345f0a866bb07b4dda438
Kei <budi03@gmail.com>
2023-02-22 23:27:26 +0700

    Update HistoryTag.php

--- app/Enums/HistoryTag.php
+++ app/Enums/HistoryTag.php
@@ -4,6 +4,9 @@ namespace App\Enums;
 
 enum HistoryTag: string
 {
+    case IssueTitle    = 'title';
+    case IssueStatus   = 'status';
+    case CommentStatus = 'comment_status';
     case Comment = 'comment';
     case Close   = 'close';
     case Open    = 'open';

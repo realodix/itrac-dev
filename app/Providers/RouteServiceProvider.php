d63aae0c639481581dd4b9a2f358c6453244bfdf
Kei <budi03@gmail.com>
2023-02-23 09:40:13 +0700

    Fix the old errors (#21)

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -35,7 +35,6 @@ class RouteServiceProvider extends ServiceProvider
         $this->routes(function () {
             Route::middleware('api')
                 ->prefix('api')
-                ->namespace('App\Http\Controllers\API')
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
